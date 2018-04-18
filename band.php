db2946bb89118cca369a086e59e32248b90dec33
fizza-A <fizza@uw.edu>
2018-04-17 17:50:50 -0700

    new file

---
+++ band.php
@@ -0,0 +1,267 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> Select Example </title>
+
+</head>
+
+<body>
+<?php
+if ($_SERVER["REQUEST_METHOD"]=="POST") {
+$con =     mysqli_connect("localhost","root","","db_bands");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  else {
+	echo "connection established <br>";
+  }  
+  
+$RS=$_POST["Radio_Service"];
+$start_freq=$_POST["freqInputLower"];
+$end_freq=$_POST["freqInputUpper"];
+$company=$_POST["Licensee"];
+
+echo "Search Criteria <br>";
+echo "Radio service: $RS <br>";
+echo "Starting frequency: $start_freq <br>";
+echo "Ending frequency: $end_freq <br>";
+echo "Licensee name: $company <br>";
+
+echo "<br> Results <br>";
+
+$sql = sprintf("SELECT * FROM bands_info WHERE Licensee LIKE '%%%s%%' OR (Radio_Service='%s' AND freqInputLower='%f' AND freqInputUpper='%f')",
+   mysqli_real_escape_string($con, $company), mysqli_real_escape_string($con, $RS), $start_freq, $end_freq);
+	
+$results=mysqli_query($con,$sql);
+
+if(mysqli_num_rows($results)>0) {
+	while($row=mysqli_fetch_array($results)) {
+		echo $row[1]. " ".$row[2]." ".$row[3]." ".$row[4]." ".$row[5];
+		echo "<br>";
+	}
+}
+
+mysqli_close($con);
+}
+?>
+
+<form name="query" method="POST" action="bands.php">
+	<div style="float: left; width: 230px;"> <!-- Column 2 start --> 
+	<div><h3><label for="frequencyForm:freqInputLower">Frequency Range:</label></h3></div>
+	<div><input id="freqInputLower" type="text" name="freqInputLower" size="10" />
+	<span style="margin-left:5px;margin-right:5px">to</span><input id="freqInputUpper" type="text" name="freqInputUpper" size="10" /> GHz
+    </div><br/>
+	<h3>Select Using Frequency Band:</h3>
+                                    <body>
+                                            <canvas id="canvas" width="100" height="200"  style="border:1px solid #d3d3d3;"></canvas> 
+                                    </body>
+<script>
+        (function() {
+            'use strict';
+            
+            
+            // Get mouse position
+            function getMousePos(c, evt) {
+                var rect = c.getBoundingClientRect();
+                return {
+                    x: evt.clientX - rect.left,
+                    y: evt.clientY - rect.top
+                };
+            };
+            
+            // Write mouse position
+            function writeMessage(c, m) {
+                var context = c.getContext('2d');
+                context.clearRect(0, 0, c.width, c.height);
+                context.font = '18pt Calibri';
+                context.fillStyle = 'purple';
+                context.fillText(m, 10, 25);
+            };
+            
+            // Make circle
+            function makeRect(context, startX,startY,width,height, color, text) {
+                context.beginPath();
+                context.rect(startX,startY,width,height);
+                context.closePath();
+                context.stroke();
+                context.fillStyle=color;
+                context.fill();
+                context.fillStyle = "black";
+                context.fillText(text,startX,startY-5);
+            };
+        
+            function makeCircle(context, x, y, color, circleSize) {
+                context.beginPath();
+                context.arc(x, y, circleSize, 0, Math.PI * 2, true);
+                context.fillStyle = color;
+                context.fill();
+                context.closePath();
+            };
+            
+            var c = document.getElementById('canvas');
+            c.width = "1000";
+            c.height = "200";
+            var context = c.getContext('2d');
+            var elements = [];
+            
+            elements.push({
+                color: 'black',
+                startX: 0,
+                startY: 20,
+                width: 70,
+                height: 100,
+                text: "3.0",
+                clicked: function() {
+                    alert('no information available');
+                }
+            },
+            {
+                color: 'pink',
+                startX: 70,
+                startY: 20,
+                width: 50,
+                height: 100,
+                text: "3.7",
+                clicked: function() {
+                    var element = document.getElementById("freqInputLower");
+                    element.value = "3.7";
+                    var element = document.getElementById("freqInputUpper");
+                    element.value = "4.2";
+                    //alert('This is 3.7 - 4.2 GHz band');
+                }
+            },
+            {
+                color: 'black',
+                startX: 120,
+                startY: 20,
+                width: 100,
+                height: 100,
+                text: "4.2",
+                clicked: function() {
+                    alert('no information available');
+                }    
+            },
+            {
+                color: 'blue',
+                startX: 220,
+                startY: 20,
+                width: 50,
+                height: 100,
+                text: "6.0",
+                clicked: function() {
+                    alert('This is 5.925 - 6.425 GHz band');
+                }    
+            },
+            {
+                color: 'black',
+                startX: 270,
+                startY: 20,
+                width: 130,
+                height: 100,
+                text: "7.1",
+                clicked: function() {
+                    alert('No information');
+                }    
+            },
+            {
+                color: 'orange',
+                startX: 400,
+                startY: 20,
+                width: 100,
+                height: 100,
+                text: "24.25",
+				 clicked: function() {
+                    var element = document.getElementById("freqInputLower");
+                    element.value = 24.25;
+                    var element = document.getElementById("freqInputUpper");
+                    element.value = 24.45;
+                }  
+            },
+            {
+                color: 'black',
+                startX: 500,
+                startY: 20,
+                width: 30,
+                height: 100,
+                text: "24.4",
+                clicked: function() {
+                    alert('No information');
+                }    
+            },
+            {
+                color: 'green',
+                startX: 530,
+                startY: 20,
+                width: 100,
+                height: 100,
+                text: "24.7",
+                clicked: function() {
+                    var element = document.getElementById("freqInputLower");
+                    element.value = 24.75;
+                    var element = document.getElementById("freqInputUpper");
+                    element.value = 25.25;
+                }    
+            },
+             {
+                color: 'black',
+                startX: 630,
+                startY: 20,
+                width: 80,
+                height: 100,
+                text: "25.5",
+                clicked: function() {
+                    alert('no information');
+                }
+            },
+            {
+                color: 'green',
+                startX: 700,
+                startY: 20,
+                width: 100,
+                height: 100,
+                text: "27.5",
+                clicked: function() {
+                    alert('This is 27.5 - 28.35 band');
+                }   
+            }
+        
+            );
+            
+            elements.forEach(function(element) {
+                var rect = new makeRect(context, element.startX, element.startY,element.width,element.height, element.color, element.text);
+                console.log(rect);
+            });
+            
+            
+            canvas.addEventListener('click', function(e) {
+                var x = e.pageX - c.offsetLeft;
+                var y = e.pageY - c.offsetTop;
+                elements.forEach(function(element){
+                    if(x > element.startX && x < (element.startX + element.width) &&
+                             y > element.startY && y < (element.startY+element.height)) {
+                        element.clicked(); 
+                     }
+                });
+            });
+            
+        })();
+        </script>
+	Licensee:<br>
+  <input type="text" name="Licensee"><br>
+    <div><h3><label for="frequencyForm:select2">Radio Service:</label></h3></div>
+	<div><select id="Radio_Service" name="Radio_Service" multiple="multiple" size="10" style="width:200px;" tabindex="13">
+	<option value="0">Select Radio Service(s):</option>
+	<option value="24 GHz Service - TZ">24 GHz Service - TZ</option>
+	<option value="Land Mobile Radiolocation - RS">Land Mobile Radiolocation - RS </option>
+</select> </div> <br />
+	<input type="submit" name="submit" value="Submit">							
+								
+</form>
+
+</body>
+
+</html>
